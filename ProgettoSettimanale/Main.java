0d6b58af05e719c4f365bb169e8fdec00d96e1ab
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-10-06 14:57:26 +0200

    test lum img e test leum e vol video
    testati : .alzaVolume();
                 .abbassaVolume();
                 .alzaLuminosita();
                 .abbassaLuminosita();
    per Immagine e Video

--- ProgettoSettimanale/Main.java
+++ ProgettoSettimanale/Main.java
@@ -3,7 +3,19 @@ package ProgettoSettimanale;
 public class Main {
     public static void main(String[] args) {
         RegistrazioneAudio bambi = new RegistrazioneAudio("cicco pasticcio", 4);
-        System.out.println(RegistrazioneAudio.volume);
+        Immagine imgagine = new Immagine("prova");
+        Video bello = new Video("video Bello", 4);
+        System.out.println(bambi.volume);
         bambi.alzaVolume();
+        bambi.abbassaVolume();
+        System.out.println(imgagine.luminonsita);
+        imgagine.alzaLuminosita();
+        imgagine.abbassaLuminosita();
+        System.out.println(bello.luminonsita);
+        System.out.println(bello.volume);
+        bello.alzaVolume();
+        bello.abbassaVolume();
+        bello.alzaLuminosita();
+        bello.abbassaLuminosita();
     }
 }
